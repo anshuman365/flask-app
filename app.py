d35d34591eeba02e12d3e9a272de5e9ef9516c85
anshuman365 <jaspreetsinghaniya64@gmail.com>
2025-02-28 17:21:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,6 +130,11 @@ def home_page():
 def admin_login():
     return render_template('admin_login.html')
 
+@app.route('/logout')
+def logout():
+    # Logout logic here
+    return redirect(url_for('home'))
+
 """
 #------------admin system part-----------------#
 
